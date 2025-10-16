2ded212827e1ed1f058ac6f83522078bc19a8510
KasumiMercury <naoki_shira@outlook.jp>
2025-10-16 23:35:36 +0900

    get lang

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,11 @@
-use windows::{
-    core::HSTRING,
-    UI::Popups::{MessageDialog, UICommand},
-};
+use windows::Globalization::ApplicationLanguages;
 
 fn main() -> windows::core::Result<()> {
-    let content = HSTRING::from("test");
-    let title = HSTRING::from("test");
-    let dialog = MessageDialog::CreateWithTitle(&content, &title)?;
-
-    let yes_label = HSTRING::from("はい");
-    let yes_command = UICommand::Create(&yes_label)?;
-    dialog.Commands()?.Append(&yes_command)?;
-
-    let no_label = HSTRING::from("いいえ");
-    let no_command = UICommand::Create(&no_label)?;
-    dialog.Commands()?.Append(&no_command)?;
-
-    dialog.SetDefaultCommandIndex(0)?;
-    dialog.SetCancelCommandIndex(1)?;
-
-    let result = dialog.ShowAsync()?.join()?;
-
-    println!("{}", result.Label()?);
+    let languages = ApplicationLanguages::Languages()?;
+    
+    for lang in languages {
+        println!("- {}", lang);
+    }
 
     Ok(())
 }
