5f99f0c69cfaae6f80c5fdebb3652a15811ae52c
KasumiMercury <naoki_shira@outlook.jp>
2025-10-17 11:52:54 +0900

    get FilStorage

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,16 @@ fn main() -> windows::core::Result<()> {
     let path_hstring = HSTRING::from(startup_path.to_string_lossy().to_string());
     println!("Startup Path: {}", path_hstring);
 
-    let _file = StorageFile::GetFileFromPathAsync(&path_hstring)?;
+    let file_async = StorageFile::GetFileFromPathAsync(&path_hstring)?;
+    match file_async.join() {
+        Ok(file) => {
+            let file_path = file.Path()?;
+            println!("StorageFile Path: {}", file_path);
+        }
+        Err(e) => {
+            println!("Error retrieving StorageFile: {}", e);
+        }
+    }
 
     let languages = ApplicationLanguages::Languages()?;
     
