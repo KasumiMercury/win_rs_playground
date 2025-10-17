ea4ab3d62c303b99bf2eba771339df9ca23f6dc9
KasumiMercury <naoki_shira@outlook.jp>
2025-10-17 10:54:40 +0900

    get FilStorage

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-use windows::Globalization::ApplicationLanguages;
+use windows::{core::HSTRING, Globalization::ApplicationLanguages, Storage::StorageFile};
 
 fn main() -> windows::core::Result<()> {
+    let startup_path = std::env::current_exe()?;
+
+    let path_hstring = HSTRING::from(startup_path.to_string_lossy().to_string());
+    println!("Startup Path: {}", path_hstring);
+
+    let _file = StorageFile::GetFileFromPathAsync(&path_hstring)?;
+
     let languages = ApplicationLanguages::Languages()?;
     
     for lang in languages {
